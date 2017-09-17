800767f866337972e5298d771568925cf4357c3c
Ulises Giacoman <ulises.giacoman@gmail.com>
2017-09-17 14:19:42 -0400

    Fix Promises and syntax

--- index.js
+++ index.js
@@ -4,30 +4,24 @@ const microCors = require('micro-cors')
 
 require('dotenv').config()
 
-const pingSlack = async (request, response)  => {
+const pingSlack = async (request, response) => {
   const { query } = parse(request.url, true)
-
   var payload = { 'channel': process.env.CHANNEL,
-                    'username': query.username,
-                    'text': query.text,
-                    'icon_emoji': ':' + query.emoji + ':'
-                  }
+    'username': query.username,
+    'text': `${query.text}`,
+    'icon_emoji': `:${query.emoji}:`
+  }
   const formBody = ['payload=' + JSON.stringify(payload)]
 
-  const response = await fetch(process.env.SLACK_URL, {
-                                method: "POST",
-                                headers: {
-                                  "cache-control": "no-cache",
-                                  "content-type": "application/x-www-form-urlencoded"
-                                },    
-                                body: formBody
-                                })
-                              .then(function(response) {
-                                return(response.text())
-                              }, function(error) {
-                                return('Couldn\'t reach slack, check if you configured your .env file correctly.')
-                              })
-return response
+  const res = await fetch(process.env.SLACK_URL, {
+    method: 'POST',
+    headers: {
+      'cache-control': 'no-cache',
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    body: formBody
+  })
+  return await res.text()
 }
 
 const cors = microCors({ allowMethods: ['GET']})
